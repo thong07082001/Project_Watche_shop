d3a79a54aa0d833f2dfd8fbfedc106eedcd99518
thong07082001 <zztieucutezz00@gmail.com>
2023-03-22 10:44:58 +0700

    fix code +database

---
+++ DOANLAPTRINHWEB/DOANLAPTRINHWEB/Controllers/RangeSliderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DOANLAPTRINHWEB.NETCore.Demos.Controllers
+{
+    public class RangeSliderController : Controller
+    {
+        // GET: RangeSlider
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
